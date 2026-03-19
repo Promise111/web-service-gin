6d69dba80e89bf3292ade3a917a98df9ba85a482
Promise <promiseihunna@gmail.com>
2026-03-19 19:23:28 +0100

    Add getAlbumByID function to retrieve album by ID and update router to handle corresponding GET requests

--- main.go
+++ main.go
@@ -36,10 +36,23 @@ func createAlbum(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", createAlbum)
+	router.POST("/albums/:id", getAlbumByID)
 	fmt.Println("Hello, World!")
 	router.Run("localhost:8080")
 }
