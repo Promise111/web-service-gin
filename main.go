0d9cda8ea84c76f3abc4f18934961831ae866b55
Promise <promiseihunna@gmail.com>
2026-03-19 19:08:51 +0100

    Add createAlbum function and update router to handle POST requests for album creation

--- main.go
+++ main.go
@@ -25,9 +25,21 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func createAlbum(c *gin.Context) {
+	var newAlbum Album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", createAlbum)
 	fmt.Println("Hello, World!")
 	router.Run("localhost:8080")
 }
